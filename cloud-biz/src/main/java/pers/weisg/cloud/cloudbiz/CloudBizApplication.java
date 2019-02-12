9d332afc84e97e405ddb6685d0ddb9dcdf88e176
weisg81 <weisangeng@126.com>
2019-02-12 22:00:51 +0800

    springcloud配置代码修改

--- cloud-biz/src/main/java/pers/weisg/cloud/cloudbiz/CloudBizApplication.java
+++ cloud-biz/src/main/java/pers/weisg/cloud/cloudbiz/CloudBizApplication.java
@@ -1,13 +1,14 @@
 package pers.weisg.cloud.cloudbiz;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.SpringCloudApplication;
 import pers.weisg.cloud.common.swagger.annotation.EnableCloudSwagger2;
 
 @EnableCloudSwagger2
-@EnableEurekaClient //本服务启动后会自动注册进eureka服务中
-@SpringBootApplication
+//@EnableEurekaClient //本服务启动后会自动注册进eureka服务中
+//@EnableCircuitBreaker//对hystrixR熔断机制的支持
+//@SpringBootApplication
+@SpringCloudApplication
 public class CloudBizApplication {
 
     public static void main(String[] args) {
